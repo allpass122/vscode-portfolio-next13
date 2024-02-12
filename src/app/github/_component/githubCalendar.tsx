27838503a82b6bab2c4d71f69dd5f4078a8bdd50
maijoe-dempsey <dempsey@maijoe.com>
2024-02-12 13:54:32 +0800

    feat: github page

---
+++ src/app/github/_component/githubCalendar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Listbox } from "@headlessui/react";
+import dayjs from "dayjs";
+import { parseAsInteger, useQueryState } from "nuqs";
+import GitHubCalendar from "react-github-calendar";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function GithubCalendar({ username, user }: { username: string; user: any }) {
+  const currentYear = dayjs().year();
+  const createdAtDate = dayjs(user.created_at);
+  const [year, setYear] = useQueryState(username, parseAsInteger.withDefault(currentYear));
+
+  return (
+    <div className="flex flex-col items-start">
+      <div className="mb-2 flex flex-row gap-2">
+        <Listbox
+          value={year}
+          onChange={setYear}
+        >
+          <Listbox.Button className="mb-1 rounded border border-teal-400 bg-teal-800 p-1 text-xs">
+            {`choose the year: ${year}`}
+          </Listbox.Button>
+          <div className="relative">
+            <Listbox.Options className="absolute left-0 top-0 rounded border border-teal-400 bg-teal-800">
+              {Array.from({ length: currentYear - createdAtDate.year() + 1 }, (_, index) => (
+                <Listbox.Option
+                  className="cursor-pointer border-b border-teal-400 p-1 text-xs last:border-0"
+                  key={index}
+                  value={currentYear - index}
+                >
+                  {currentYear - index}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </div>
+        </Listbox>
+      </div>
+      <GitHubCalendar
+        username={username}
+        year={year}
+      />
+    </div>
+  );
+}
+
+export default GithubCalendar;
--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,5 +1,89 @@
-function GithubPage() {
-  return <h2>This is Github Page.</h2>;
+import Image from "next/image";
+import GithubCalendar from "@/app/github/_component/githubCalendar";
+
+async function GithubPage() {
+  const { user } = await getData();
+  return (
+    <div className="flex grow flex-col bg-dark-second p-8">
+      <div className="flex flex-row flex-wrap gap-2">
+        <div className="rounded-lg bg-dark-primary p-4">
+          <div>
+            <Image
+              className="my-4 rounded-full"
+              src={user.avatar_url}
+              width={160}
+              height={160}
+              alt={"avatar"}
+            />
+          </div>
+          <a
+            className="font-inter text-gray-400 "
+            href={"https://github.com/dempsey1229"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {user.login}
+          </a>
+        </div>
+        <div className="font-inter flex items-center rounded-lg bg-dark-primary p-4">
+          <GithubCalendar
+            username={process.env.NEXT_PUBLIC_GITHUB_USERNAME as string}
+            user={user}
+          />
+          {/* <div className=""> {user.public_repos} </div> */}
+          {/* <div className="">{user.followers}</div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+async function getData() {
+  // const requestBody = {
+  //   query: `query($userName:String!) {
+  //     user(login: $userName){
+  //       contributionsCollection {
+  //         contributionCalendar {
+  //           totalContributions
+  //           weeks {
+  //             contributionDays {
+  //               contributionCount
+  //               date
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }`,
+  //   variables: { userName: process.env.NEXT_PUBLIC_GITHUB_USERNAME },
+  // };
+
+  // const calendarRes = await fetch("https://api.github.com/graphql", {
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: `token ${process.env.GITHUB_API_KEY}`,
+  //   },
+  //   body: JSON.stringify(requestBody),
+  // });
+  // const calendar = await calendarRes.json();
+  // console.log(calendar.data.user.contributionsCollection);
+
+  const userRes = await fetch(
+    `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME}`,
+    {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_API_KEY}`,
+      },
+    }
+  );
+  if (!userRes.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const user = await userRes.json();
+  // console.log(user);
+  return {
+    user,
+  };
 }
 
 export default GithubPage;
