1e30891d087200c2870853aa7dc0cfd6977e643d
maijoe-dempsey <dempsey@maijoe.com>
2024-02-14 01:51:25 +0800

    feat: add animation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,7 +10,8 @@ function HomePage() {
           flag === 1 && "bg-red-400",
           flag === 2 && "bg-green-400",
           flag === 3 && "bg-blue-400",
-          flag === 4 && "bg-yellow-400"
+          flag === 4 && "bg-yellow-400",
+          "flip-in-ver-right"
         )}
       >
         <span className="whitespace-pre-line text-xl text-gray-700">{text}</span>
@@ -23,7 +24,8 @@ function HomePage() {
       <div className="flex size-fit flex-col gap-4 rounded-br-3xl border-b-8 border-r-8 border-orange-800 p-2 font-serif">
         <div className="flex flex-row gap-4">
           <TextBlock
-            text={`Hello ~ This is home page.
+            text={`Hello ~ 
+            This is home page.
         This is my first side-project, I hope you like it.`}
             flag={1}
           />
