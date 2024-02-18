67fcc9a681bd48d18ee4e2a55942b9316cd8ffe7
maijoe-dempsey <dempsey@maijoe.com>
2024-02-19 02:46:07 +0800

    refactor: theme controller position move into header

--- src/app/_component/headerBar.tsx
+++ src/app/_component/headerBar.tsx
@@ -1,6 +1,52 @@
 import Image from "next/image";
 
 function HeaderBar() {
+  function ThemeController() {
+    return (
+      <div className="dropdown">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-xs btn-outline  btn-accent"
+        >
+          Theme
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
+        >
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Default"
+              value="default"
+            />
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Dark"
+              value="dark"
+            />
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Light"
+              value="light"
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div className="border-base-100 bg-base-100 flex h-8 select-none items-center border-b-[0.5px] px-2">
       <Image
@@ -9,7 +55,9 @@ function HeaderBar() {
         height={16}
         width={16}
       />
-      <div className="ml-2 mr-auto flex flex-auto flex-wrap content-center gap-1">
+      <div className="my-0.5 ml-2 mr-auto flex flex-auto flex-wrap content-center gap-1">
+        <ThemeController />
+
         {["File", "Edit", "View", "Go", "Run", "Terminal", "Help"].map((text) => (
           <p
             className="btn btn-xs"
@@ -19,6 +67,7 @@ function HeaderBar() {
           </p>
         ))}
       </div>
+
       <p className="prose font-mono center flex flex-auto text-center text-sm">
         {"Dempsey Huang - Visual Studio Code"}
       </p>
